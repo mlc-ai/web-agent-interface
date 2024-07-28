46eba8cc6331909819b4baa6bf42c45b5ea37cd9
YiyanZhai <zhaiyiyan17@gmail.com>
2024-07-27 21:52:42 -0400

    debug overleafPage's getTextSelection Method

--- src/page/googleDocPage.js
+++ src/page/googleDocPage.js
@@ -1,55 +1,63 @@
 import { Page } from './page';
 import { Action } from '../action';
 
-// () -> string
-function getTextSelectionImpl() { 
-    if (! window.location.toString().includes("google.com/docs")) {  // If not on the correct page, return empty string
-        console.log("Not on google doc page");
+/**
+ * Implementation of getTextSelection and replaceSelectedText for Google Docs
+ * @class GoogleDocPage
+ */
+export class GoogleDocPage extends Page {
+    // Private methods
+    getTextSelectionImpl() { 
+        if (! window.location.toString().includes("docs.google.com")) {
+            console.log("Not on google doc page");
+        }
+        const selection = window.getSelection();
+        if (selection.type === "Range") {
+            console.log("Selected text:", selection.toString());
+            return selection.toString();
+        } else {
+            console.log("No text selection");
+            return "";
+        }
     }
-    const selection = window.getSelection();
-    if (selection.type == "Range") {
-        console.log("Selected text:", selection.toString());
-        return selection.toString();
-    } else {
-        console.log("No text selection");
-        return "";
-    }
-}
 
-// string -> void
-function replaceSelectedTextImpl(newText) {
-    const nodes = Array.from(document.getElementsByClassName("cm-line")).filter( el => {
-        const cleanText = el.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
-        if (cleanText.length > 0) {
-            if (selectedText.includes(cleanText)) {
-                return true;
+    replaceSelectedTextImpl(newText) {
+        // the following implementation is from mlc-ai/mlc-assistant
+        const nodes = Array.from(document.getElementsByClassName("cm-line")).filter( el => {
+            const cleanText = el.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
+            if (cleanText.length > 0) {
+                if (selectedText.includes(cleanText)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        console.log("Replacing Selected Text", nodes)
+
+        // Replace content of first node with new text, and remove all subsequent nodes
+        if (nodes.length > 0) {
+            nodes[0].innerHTML = newText;
+            if (nodes.length > 1) {
+                nodes.slice(1).forEach( node => {
+                    node.remove();
+                });
             }
         }
-        return false;
-    });
-    console.log("Replacing Selected Text", nodes)
+    }
 
-    // Replace content of first node with new text, and remove all subsequent nodes
-    if (nodes.length > 0) {
-        nodes[0].innerHTML = newText;
-        if (nodes.length > 1) {
-            nodes.slice(1).forEach( node => {
-                node.remove();
-            });
+    // use this method to execute actions: overleafPageInstance.executeAction('getTextSelection');
+    executeAction(actionName, ...args) {
+        const action = this.nameToAction[actionName];
+        if (action) {
+            return action.call(...args);
+        } else {
+            console.log(`Action '${actionName}' not found.`);
         }
     }
+
+    nameToAction = {
+        'getTextSelection': new Action(this.getTextSelectionImpl),
+        'replaceSelectedText': new Action(this.replaceSelectedTextImpl)
+    };
+    availableActions = Object.keys(this.nameToAction);
 }
-
-nameToAction = {
-    'getTextSelection': new Action(getTextSelectionImpl),
-    'replaceSelectedText': new Action(replaceSelectedTextImpl)
-};
-
-availableActions = [
-    'getTextSelection',
-    'replaceSelectedText'
-];
-
-GoogleDocPage = new Page(nameToAction, availableActions);
-
-export { GoogleDocPage };
