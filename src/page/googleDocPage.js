68e8fed09309050499e8e3a9ece3f028ae605e36
YiyanZhai <zhaiyiyan17@gmail.com>
2024-08-01 13:22:54 -0400

    fix replaceSelectedTextImpl for OverleafPage

--- src/page/googleDocPage.js
+++ src/page/googleDocPage.js
@@ -6,15 +6,17 @@ import { Action } from '../action';
  * @class GoogleDocPage
  */
 export class GoogleDocPage extends Page {
-    // Private methods
     getTextSelectionImpl() { 
-        if (! window.location.toString().includes("docs.google.com")) {
-            console.log("Not on google doc page");
-        }
-        const selection = window.getSelection();
-        if (selection.type === "Range") {
-            console.log("Selected text:", selection.toString());
-            return selection.toString();
+        // if (! window.location.toString().includes("docs.google.com")) {
+        //     console.log("Not on google doc page");
+        // }
+        // use Google Add-on script to get selected text
+        // https://developers.google.com/apps-script/reference/document/text#gettext
+        const selection = DocumentApp.getActiveDocument().getSelection();
+        if (selection) {
+            const text = selection.getText();
+            console.log("Selected text:", text);
+            return text;
         } else {
             console.log("No text selection");
             return "";
@@ -22,30 +24,20 @@ export class GoogleDocPage extends Page {
     }
 
     replaceSelectedTextImpl(newText) {
-        // the following implementation is from mlc-ai/mlc-assistant
-        const nodes = Array.from(document.getElementsByClassName("cm-line")).filter( el => {
-            const cleanText = el.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
-            if (cleanText.length > 0) {
-                if (selectedText.includes(cleanText)) {
-                    return true;
-                }
-            }
-            return false;
-        });
-        console.log("Replacing Selected Text", nodes)
-
-        // Replace content of first node with new text, and remove all subsequent nodes
-        if (nodes.length > 0) {
-            nodes[0].innerHTML = newText;
-            if (nodes.length > 1) {
-                nodes.slice(1).forEach( node => {
-                    node.remove();
-                });
-            }
+        if (! window.location.toString().includes("docs.google.com")) {
+            console.log("Not on google doc page");
+        }
+        // use Google Add-on script to replace selected text
+        // https://developers.google.com/apps-script/reference/document/text#settexttext
+        const selection = DocumentApp.getActiveDocument().getSelection();
+        if (selection) {
+            selection.replaceText(newText);
+        } else {
+            console.log("No text selection for replacement");
         }
     }
 
-    // use this method to execute actions: overleafPageInstance.executeAction('getTextSelection');
+    // use this method to execute actions: gdocPageInstance.executeAction('getTextSelection');
     executeAction(actionName, ...args) {
         const action = this.nameToAction[actionName];
         if (action) {
