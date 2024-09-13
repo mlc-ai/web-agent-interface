8eaf5ede4dfad8c722a8a5de5cc71498d18c0ce7
Nestor Qin <imba.qxy@gmail.com>
2024-09-12 23:29:27 -0400

    feat: Add tool description and schema

--- src/page/overleaf.ts
+++ src/page/overleaf.ts
@@ -44,7 +44,7 @@ export class PageHandler implements IPageHandler {
       if (Array.isArray(newText)) {
         const fragment = document.createDocumentFragment();
         newText.forEach((text) =>
-          fragment.appendChild(document.createTextNode(text))
+          fragment.appendChild(document.createTextNode(text)),
         );
         range.insertNode(fragment);
       } else {
@@ -88,12 +88,56 @@ export class PageHandler implements IPageHandler {
 export const tools = {
   getSelection: {
     displayName: "Get Selected Text",
+    description:
+      "Get the user's current selected text content on the document.",
+    schema: {
+      type: "function",
+      function: {
+        name: "getSelection",
+        description:
+          "getSelection() -> str - Get the user's current selected text content on the document, no parameter is needed.\\n\\n Returns:\\n    str: The user's current selected text content on the document.",
+        parameters: { type: "object", properties: {}, required: [] },
+      },
+    },
   },
   replaceSelection: {
     displayName: "Replace Selected Text",
+    description:
+      "Replace the user's current selected text content on the document with new text content.",
+    schema: {
+      type: "function",
+      function: {
+        name: "replaceSelection",
+        description:
+          "replaceSelection(newText: str) - Replace the user's current selected text content on the document with new text content.\\n\\n Args:\\n    newText (str): New text content to replace the user's current selected text content.",
+        parameters: {
+          type: "object",
+          properties: {
+            newText: { type: "string" },
+          },
+          required: ["newText"],
+        },
+      },
+    },
   },
   appendText: {
     displayName: "Append New Text",
+    description: "Add some text content to the end of the document.",
+    schema: {
+      type: "function",
+      function: {
+        name: "appendText",
+        description:
+          "appendText(text: str) - Add some text content to the end of the document.\\n\\n Args:\\n    text (str): Text content to be added to the end of the document.",
+        parameters: {
+          type: "object",
+          properties: {
+            text: { type: "string" },
+          },
+          required: ["text"],
+        },
+      },
+    },
   },
 };
 
