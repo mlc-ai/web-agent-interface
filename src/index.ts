f0e643ac7b27f6dc4da7913aa438d58d42b1edbb
Nestor Qin <imba.qxy@gmail.com>
2024-09-13 23:30:00 -0400

    feat: Export isCurrentPageSupported

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { IPageHandler } from "./page/interface";
 import * as Oveleaf from "./page/overleaf";
 
 const PAGE_HANDLER_MAP: Record<string, typeof Oveleaf.PageHandler> = {
@@ -9,15 +8,19 @@ const PAGE_TOOLS_MAP: Record<string, typeof Oveleaf.tools> = {
   "www.overleaf.com": Oveleaf.tools,
 };
 
+export const isCurrentPageSupported = () => {
+  return Object.keys(PAGE_HANDLER_MAP).includes(window.location.hostname);
+};
+
 export const initHandler = () => {
-  if (Object.keys(PAGE_HANDLER_MAP).includes(window.location.hostname)) {
+  if (isCurrentPageSupported()) {
     return new PAGE_HANDLER_MAP[window.location.hostname]();
   }
   console.error("[Web Agent Interface] No tools found for the current page");
 };
 
 const getTools = () => {
-  if (Object.keys(PAGE_TOOLS_MAP).includes(window.location.hostname)) {
+  if (isCurrentPageSupported()) {
     return PAGE_TOOLS_MAP[window.location.hostname];
   }
   console.error("[Web Agent Interface] No tools found for the current page");
