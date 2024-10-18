11de7425a297bacd79e7d60155d824dae63a1cfc
Nestor Qin <imba.qxy@gmail.com>
2024-10-18 02:55:16 -0400

    feat: refator State constructor

--- src/state.ts
+++ src/state.ts
@@ -1,10 +1,26 @@
+export enum Scope {
+  DOM = "DOM",
+}
+
 export class State {
   public currentSelection: Selection | undefined;
+  private scopeRegistration = {
+    DOM: this.registerDOM,
+  };
 
-  constructor() {
+  constructor(scopes: Scope[]) {
+    this.registerScopes(scopes);
+  }
+
+  private registerScopes(scopes: Scope[]) {
+    scopes.forEach((scope) => {
+      this.scopeRegistration[scope]();
+    });
+  }
+
+  private registerDOM() {
     document.addEventListener("selectionchange", (): void => {
       const selection = window.getSelection();
-
       if (
         selection &&
         typeof selection.rangeCount !== "undefined" &&
